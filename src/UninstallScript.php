b32a0af8631af5df977db5a063b44e33ae4f3fe2
zds <49744633+zds-s@users.noreply.github.com>
2025-02-24 14:54:22 +0800

    first commit

---
+++ src/UninstallScript.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Plugin\MineAdmin\MineAdmin\Crontab;
+
+class InstallScript {
+
+    public function __invoke(){
+        echo "Commands to be executed when uninstalling the plug-in";
+    }
+
+}
