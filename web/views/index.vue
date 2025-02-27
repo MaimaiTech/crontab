e82723ba41f8c3e7976fb61a850c170a1b8bb52c
X.Mo <root@imoi.cn>
2025-02-27 16:14:47 +0800

    前端

---
+++ web/views/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineOptions({ name: 'mine:crontab' })
+</script>
+
+<template>
+  <div class="mine-card">
+    asda
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
