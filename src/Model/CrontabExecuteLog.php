cd55f5e79bab350e2e8fc7fc7c62bf5a64a9f0bb
zds <49744633+zds-s@users.noreply.github.com>
2025-07-10 14:22:38 +0800

    fix: update exception_info property type from array to string in CrontabExecuteLog

--- src/Model/CrontabExecuteLog.php
+++ src/Model/CrontabExecuteLog.php
@@ -11,7 +11,7 @@ use Hyperf\DbConnection\Model\Model;
  * @property string $name 任务名称
  * @property int $status 执行状态 0:失败 1:成功
  * @property string $target 执行目标
- * @property array $exception_info 异常信息
+ * @property string $exception_info 异常信息
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property-read Crontab $crontab
@@ -28,7 +28,7 @@ class CrontabExecuteLog extends Model
     protected array $casts = [
         'crontab_id' => 'int',
         'status' => 'int',
-        'exception_info' => 'array',
+        'exception_info' => 'string',
         'created_at' => 'datetime',
         'updated_at' => 'datetime'
     ];
